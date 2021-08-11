0444a08f64e1e6c88f1d631e48d14a266ed2ea3a
Win Thawin <thawinmail@gmail.com>
2021-08-11 16:50:22 +0700

    Ex 4

---
+++ assignments/Exercise4_ธาวิน_W.py
@@ -0,0 +1,14 @@
+Foundation_English = input ('Enter your \"Foundation English\" score: ')
+
+General_Business = input ("Enter your \"General Business\" score: ")
+
+Introduction_to_Computer_Systems = input ("Enter your \"Introduction to Computer Systems\" score: ")
+
+Computer_Programming = input ("Enter your \"Computer Programming\" score: ")
+
+print("----------Score----------")
+print("Foundation English: ", Foundation_English,"point")
+print("General Business: ", General_Business,"point")
+print("Introduction to Computer Systems: ", Introduction_to_Computer_Systems,"point")
+print("Computer Programming: ", Computer_Programming,"point")
+
